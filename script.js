94fa74c20adf87ca458cf681f358fc9807fd42c3
sagarsinghthakur002 <sagarsinghthakur002@gmail.com>
2024-09-18 22:10:18 +0545

    feat: some micro update

--- script.js
+++ script.js
@@ -6,14 +6,28 @@ let secItem = document.querySelector("#sec");
 let countDown = () => {
 
     setInterval (() => {
+
     let futureDate = new Date("1 Jan 2025"); 
+
     let currentDate = new Date();
+
     let timeLeft = futureDate - currentDate;
 
+    // fut - curr time 
     let days = Math.floor(timeLeft / 1000 / 60 / 60 / 24);
+       
     let hours = Math.floor(timeLeft / 1000 / 60 / 60) % 24;
+    
     let minutes = Math.floor(timeLeft / 1000 / 60) % 60; 
+
     let seconds = Math.floor(timeLeft / 1000) % 60;
+
+    
+       // Divide by 1000 to convert milliseconds into seconds.
+       // Divide by 60 to convert seconds into minutes.
+       // Divide by 60 again to convert minutes into hours.
+       // Finally, divide by 24 to convert hours into days.
+    
 
     daysItem.innerHTML = days;
     hoursItem.innerHTML = hours;
